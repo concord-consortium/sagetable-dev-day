07589e53418a72830ba43211c5c8c8c751ddc852
Eli Kosminsky <ekosminsky@concord.org>
2019-05-02 11:28:37 -0400

    Drawing circles

--- src/video/capture.js
+++ src/video/capture.js
@@ -5,10 +5,11 @@ const width = 300;
 const height = 225;
 const FPS = 30;
 let stream;
-let streaming = false;
 
-let src;
-let dst;
+let srcMat = new cv.Mat(height, width, cv.CV_8UC4);
+let grayMat = new cv.Mat(height, width, cv.CV_8UC1);
+let circlesMat = new cv.Mat();
+
 const cap = new cv.VideoCapture(video);
 
 export default function capture() {
@@ -17,23 +18,33 @@ export default function capture() {
       stream = _stream;
       video.srcObject = stream;
       video.play();
-      streaming = true;
-      src = new cv.Mat(height, width, cv.CV_8UC4);
-      dst = new cv.Mat(height, width, cv.CV_8UC1);
       setTimeout(processVideo, 0)
   })
   .catch(err => console.log(`An error occurred: ${err}`));
 
   function processVideo () {
-    if (!streaming) {
-        src.delete();
-        dst.delete();
-        return;
-    }
     const begin = Date.now();
-    cap.read(src)
-    cv.cvtColor(src, dst, cv.COLOR_RGBA2GRAY);
-    cv.imshow('canvasOutput', dst);
+
+    cap.read(srcMat);
+
+    cv.cvtColor(srcMat, grayMat, cv.COLOR_RGBA2GRAY);
+
+    let displayMat = new cv.Mat(height, width, cv.CV_8UC4, cv.Scalar.all(0));
+    
+    cv.HoughCircles(grayMat, circlesMat, cv.HOUGH_GRADIENT, 1, 45, 75, 40, 0, 0);
+
+    for (let i = 0; i < circlesMat.cols; ++i) {
+        let x = circlesMat.data32F[i * 3];
+        let y = circlesMat.data32F[i * 3 + 1];
+        let radius = circlesMat.data32F[i * 3 + 2];
+
+        let center = new cv.Point(x, y);
+        cv.circle(displayMat, center, radius, [255, 0, 0, 255], 3);
+    }
+
+    cv.imshow('canvasOutput', displayMat);
+
+
     const delay = 1000/FPS - (Date.now() - begin);
     setTimeout(processVideo, delay);
   }
